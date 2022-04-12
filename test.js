d68b37bf65685b4c01091236e400b69860321a9c
LuckyForce <adrian.schauer@aon.at>
2022-04-12 22:11:56 +0200

    JS: Add Test Script.

---
+++ test.js
@@ -0,0 +1,18 @@
+//import lang from 'EasyLang/lang.js';
+import lang from 'lang.js';
+
+//Print all test templates. HelloWorld, Welcome and Goodbye
+console.log("English");
+console.log(lang.getPhrase('HelloWorld', 'en'));
+console.log(lang.getPhrase('Welcome', 'en'));
+console.log(lang.getPhrase('Goodbye', 'en'));
+console.log("");
+console.log("German");
+console.log(lang.getPhrase('HelloWorld', 'de'));
+console.log(lang.getPhrase('Welcome', 'de'));
+console.log(lang.getPhrase('Goodbye', 'de'));
+console.log("");
+console.log("Spanish");
+console.log(lang.getPhrase('HelloWorld', 'es'));
+console.log(lang.getPhrase('Welcome', 'es'));
+console.log(lang.getPhrase('Goodbye', 'es'));
