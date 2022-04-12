f0e3416e0adad25b68bc9a16b9a74ac39f97de0b
LuckyForce <adrian.schauer@aon.at>
2022-04-12 21:56:06 +0200

    JS: First Prototype.

---
+++ lang.js
@@ -0,0 +1,34 @@
+class lang {
+    static JSON_PATH = 'lang.json';
+
+    //Constructor
+    constructor() {
+        //Load json file
+        this.json = require(lang.JSON_PATH);
+    }
+
+    //Get Phrase for the given language
+    getPhrase(phrase, lang) {
+        //Check if phrase exists
+        if (this.json[phrase] == undefined) {
+            //Return null
+            return null;
+        } else {
+            //Check if language exists
+            if (this.json[phrase][lang] == undefined) {
+                //Check if default language exists
+                if (this.json[phrase][this.json['default']] == undefined) {
+                    //Return null
+                    return null;
+                } else {
+                    //Return default language
+                    return this.json[phrase][this.json['default']];
+                }
+            } else {
+                //Return language
+                return this.json[phrase][lang];
+            }
+        }
+    }
+}
+
