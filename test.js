96446a80802e7d14cada3884da2a0730edf5f796
LuckyForce <adrian.schauer@aon.at>
2022-04-12 22:19:32 +0200

    JS: Rename to Lang in Test File.

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 //import lang from 'EasyLang/lang.js';
-import lang from 'lang.js';
+import Lang from 'lang.js';
 
 //Print all test templates. HelloWorld, Welcome and Goodbye
 console.log("English");
-console.log(lang.getPhrase('HelloWorld', 'en'));
-console.log(lang.getPhrase('Welcome', 'en'));
-console.log(lang.getPhrase('Goodbye', 'en'));
+console.log(Lang.getPhrase('HelloWorld', 'en'));
+console.log(Lang.getPhrase('Welcome', 'en'));
+console.log(Lang.getPhrase('Goodbye', 'en'));
 console.log("");
 console.log("German");
-console.log(lang.getPhrase('HelloWorld', 'de'));
-console.log(lang.getPhrase('Welcome', 'de'));
-console.log(lang.getPhrase('Goodbye', 'de'));
+console.log(Lang.getPhrase('HelloWorld', 'de'));
+console.log(Lang.getPhrase('Welcome', 'de'));
+console.log(Lang.getPhrase('Goodbye', 'de'));
 console.log("");
 console.log("Spanish");
-console.log(lang.getPhrase('HelloWorld', 'es'));
-console.log(lang.getPhrase('Welcome', 'es'));
-console.log(lang.getPhrase('Goodbye', 'es'));
+console.log(Lang.getPhrase('HelloWorld', 'es'));
+console.log(Lang.getPhrase('Welcome', 'es'));
+console.log(Lang.getPhrase('Goodbye', 'es'));
